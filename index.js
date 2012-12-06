ba6a40bad56830db5bdab4c54b3cd22f8723b377
Curtis Lacy <curtis.lacy@ldengine.com>
2012-12-05 22:51:01 -0500

    Refactored the regex matching and invocation of a command line into a utility method.  Also made the terminate instance method verify that the instance has been removed before invoking its callback.

--- index.js
+++ index.js
@@ -10,9 +10,28 @@ var elb_bin = process.env.AWS_ELB_HOME + '/bin/';
 
 function Ec2() {
 }
+Ec2.prototype._useCli = function( command, args, parsingRegex, objectParser, callback ) {
+	( require( './lib/SpawnProcess' ) )( 
+		command, args,
+		function( output ) {
+			var results = [];
+			var dataLines = output.match( new RegExp( parsingRegex, 'g' ) );
+			dataLines.forEach( function( line ) {
+				var matches = line.match( new RegExp( parsingRegex, '' ) );
+				if( matches )
+					results.push( objectParser( matches ) );
+			});
+			callback( null, results );
+		},
+		function( errors ) {
+			callback( errors, null );
+		}
+	);		
+}
 //./mon-get-stats CPUUtilization --namespace "AWS/EC2" --statistics "Minimum,Maximum,Average" --headers --period 60 --dimensions "InstanceId=i-37211948"
 Ec2.prototype.getAverageCPUUtilization = function( region, instanceId, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	var newestEntry = null;
+	this._useCli( 
 		cloudwatch_bin + 'mon-get-stats',
 		[ 
 			'CPUUtilization',
@@ -21,132 +40,110 @@ Ec2.prototype.getAverageCPUUtilization = function( region, instanceId, callback 
 			'--headers' ,
 			'--period', 60,
 			'--region', region,
-			'--dimensions', '"InstanceId=' + instanceId + '"' ],
-		function( output ) {
-			var newestEntry = null;
-
-			var dataLines = output.match( /[0-9\-]+ [0-9:]+ +[0-9\.]+ +Percent/g );
-			dataLines.forEach( function( line ) {
-				var splitData = line.match( /([0-9\-]+ [0-9:]+) +([0-9\.]+) +Percent/ );
-
-				var date = moment( splitData[1] );
-				if( !newestEntry || date.valueOf() > newestEntry.timestamp )
-					newestEntry = {
-						"timestamp": date.valueOf(),
-						"percent": splitData[2]
-					};
-			});
-			
-			callback( null, newestEntry.percent );
+			'--dimensions', '"InstanceId=' + instanceId + '"' 
+		],
+		'([0-9\-]+ [0-9:]+) +([0-9\.]+) +Percent',
+		function( matches ) {
+			var date = moment( matches[1] );
+			if( !newestEntry || date.valueOf() > newestEntry.timestamp )
+				newestEntry = {
+					"timestamp": date.valueOf(),
+					"percent": matches[2]
+				};
+			return 0;
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);	
+		function( error, result ) {
+			callback( error, newestEntry.percent );
+		} );
 }
 Ec2.prototype.getElasticLoadBalancers = function( region, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	this._useCli( 
 		elb_bin + 'elb-describe-lbs',
 		[ 
 			'--region', region
 		],
-		function( output ) {
-			var balancers = [];
-			var dataLines = output.match( /LOAD_BALANCER +[A-Za-z0-9\-]+ +[A-Za-z0-9\-\.]+/g );
-			dataLines.forEach( function( line ) {
-				var matches = line.match( /LOAD_BALANCER +([A-Za-z0-9\-]+) +([A-Za-z0-9\-\.]+)/ );
-				var info = {
-					"name": matches[1],
-					"external": matches[2]
-				};
-				balancers.push( info );
-			});
-			callback( null, balancers );
+		'LOAD_BALANCER +([A-Za-z0-9\-]+) +([A-Za-z0-9\-\.]+)',
+		function( matches ) {
+			return {
+				"name": matches[1],
+				"external": matches[2]
+			}
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);		
+		callback );
 }
 Ec2.prototype.getBalancedInstances = function( region, balancerName, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	this._useCli(
 		elb_bin + 'elb-describe-instance-health',
 		[ 
 			balancerName,
 			'--region', region
 		],
-		function( output ) {
-			var instances = [];
-			var dataLines = output.match( /INSTANCE_ID +[A-Za-z0-9\-]+ +[A-Za-z0-9\-\.]+/g );
-			dataLines.forEach( function( line ) {
-				var matches = line.match( /INSTANCE_ID +([A-Za-z0-9\-]+) +([A-Za-z0-9\-\.]+)/ );
-				var info = {
-					"instance": matches[1],
-					"state": matches[2]
-				};
-				instances.push( info );
-			});
-			callback( null, instances );
+		'INSTANCE_ID +([A-Za-z0-9\-]+) +([A-Za-z0-9\-\.]+)',
+		function( matches ) {
+			return {
+				"instance": matches[1],
+				"state": matches[2]
+			};
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);		
+		callback );		
 }
 Ec2.prototype.getRunningInstances = function( region, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	this._useCli(
 		ec2_bin + 'ec2-describe-instances',
 		[ '--region', region ],
-		function( output ) {
-			var instances = [];
-			var dataLines = output.match( /INSTANCE\t[A-Za-z0-9\-]+\t[A-Za-z0-9\-]+\t[A-Za-z0-9\-\.]+\t[A-Za-z0-9\-\.]+/g );
-			dataLines.forEach( function( line ) {
-				var matches = line.match( /INSTANCE\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-\.]+)\t([A-Za-z0-9\-\.]+)/ );
-				var info = {
-					"instance": matches[1],
-					"ami": matches[2],
-					"external": matches[3],
-					"internal": matches[4]
-				};
-				instances.push( info );
-			});
-			callback( null, instances );
+		'INSTANCE\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-\.]+)\t([A-Za-z0-9\-\.]+)',
+		function( matches ) {
+			return {
+				"instance": matches[1],
+				"ami": matches[2],
+				"external": matches[3],
+				"internal": matches[4]
+			};
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);
+		callback );
 }
 Ec2.prototype.getAMIs = function( region, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	this._useCli(
 		ec2_bin + 'ec2-describe-images',
 		[ '--region', region ],
-		function( output ) {
-			var images = [];
-			var dataLines = output.match( /IMAGE\t[A-Za-z0-9\-]+\t[A-Za-z0-9\-\/ ]+\t[A-Za-z0-9\-]+\t[A-Za-z0-9\-]+/g );
-			console.log( dataLines );
-			dataLines.forEach( function( line ) {
-				var matches = line.match( /IMAGE\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-\/ ]+)\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-]+)/ );
-				var info = {
-					"ami": matches[1],
-					"name": matches[2],
-					"state": matches[4]
-				};
-				images.push( info );
-			});
-			callback( null, images );
+		'IMAGE\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-\/ ]+)\t([A-Za-z0-9\-]+)\t([A-Za-z0-9\-]+)',
+		function( matches ) {
+			return {
+				"ami": matches[1],
+				"name": matches[2],
+				"state": matches[4]
+			};
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);
+		callback );
 }
 Ec2.prototype.terminateInstance = function( region, instance, callback ) {
+	var self = this;
 	( require( './lib/SpawnProcess' ) )( 
 		ec2_bin + 'ec2-terminate-instances',
 		[ '--region', region, instance ],
 		function( output ) {
-			callback( null );
+			var retry = 0;
+			function checkTerminated() {
+				self.getRunningInstances( region, function( error, instances ) {
+					var found = false;
+					instances.forEach( function( foundInstance ) {
+						if( foundInstance.instance == instance )
+							found = true;
+					});
+
+					if( !found )
+						callback( null );
+					else
+					{
+						retry++;
+						if( retry < 12 )
+							setTimeout( checkTerminated, 5000 );
+						else
+							callback( 'Instance took too long to terminate: ' + instance );
+					}
+				});
+			}
+			setTimeout( checkTerminated, 5000 );
 		},
 		function( errors ) {
 			callback( errors );
@@ -154,24 +151,15 @@ Ec2.prototype.terminateInstance = function( region, instance, callback ) {
 	);	
 }
 Ec2.prototype.launchInstance = function( region, image, keypair, type, callback ) {
-	( require( './lib/SpawnProcess' ) )( 
+	this._useCli(
 		ec2_bin + 'ec2-run-instances',
 		[ '--region', region, '-n', 1, '-k', keypair, '-t', type, image ],
-		function( output ) {
-			var instances = [];
-			var dataLines = output.match( /INSTANCE\t[A-Za-z0-9\-]+/g );
-			dataLines.forEach( function( line ) {
-				var matches = line.match( /INSTANCE\t([A-Za-z0-9\-]+)/ );
-				var info = {
-					"instance": matches[1],
-				};
-				instances.push( info );
-			});
-			callback( null, instances );
+		'INSTANCE\t([A-Za-z0-9\-]+)',
+		function( matches ) {
+			return {
+				"instance": matches[1],
+			};
 		},
-		function( errors ) {
-			callback( errors, null );
-		}
-	);	
+		callback );
 }
 module.exports = exports = new Ec2();
