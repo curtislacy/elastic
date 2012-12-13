0b67dd8e1618a9881781071e5dfb45c21b699085
Curtis Lacy <curtis.lacy@ldengine.com>
2012-12-13 10:28:56 -0500

    Started adding an optional logger to the various components.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 module.exports.ec2Client = require( './lib/Ec2Client' );
 module.exports.LoadBalancedCluster = require( './lib/LoadBalancedCluster' );
+
+module.exports.setLogger = function( logger ) {
+	module.exports.ec2Client.setLogger( logger );
+	module.exports.LoadBalancedCluster.setLogger( logger );
+}
+
+module.exports.setLogger( {
+		log: function( obj ) {
+			console.log( 'l: ' + obj );
+		}
+	};
+)
